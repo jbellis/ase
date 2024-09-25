df92d9a574ba9e4452a5d4bf796691343e15a295
Jonathan Ellis <jbellis@datastax.com>
2024-09-25 10:51:38 -0500

    update astrapy usage (untested)

--- db.py
+++ db.py
@@ -1,13 +1,12 @@
 import os
-
-from astrapy.db import AstraDB, AstraDBCollection
+from astrapy import DataAPIClient
+from astrapy.constants import VectorMetric
 
 from util import hexdigest
 
 # set up api endpoint and secret token
-_db = AstraDB(token=os.environ["ASTRA_DB_APPLICATION_TOKEN"],
-              api_endpoint=os.environ["ASTRA_DB_API_ENDPOINT"],
-              namespace="default_keyspace",)
+_client = DataAPIClient(token=os.environ["ASTRA_DB_TOKEN"])
+_db = _client.get_database(os.environ["ASTRA_DB_ID"])
 
 _embeddings = None
 _files = None
@@ -19,37 +18,37 @@ def init(collection_name):
     """
     global _embeddings, _files
     print('Connecting to database for collection ' + collection_name)
-    # get_collections is fast enough that I haven't bothered to optimize this further with local caching
-    all_collections = set(_db.get_collections()['status']['collections'])
-
+    
     embeddings_collection_name = collection_name + "_embeddings"
-    if embeddings_collection_name in all_collections:
-        _embeddings = AstraDBCollection(embeddings_collection_name, _db)
-    else:
-        _embeddings = _db.create_collection(embeddings_collection_name, dimension=768, metric="cosine",
-                                            options={'indexing': {'deny': ['chunk']}})
-
     files_collection_name = collection_name + "_files"
-    if files_collection_name in all_collections:
-        _files = AstraDBCollection(files_collection_name, _db)
+    
+    collections = _db.list_collections()
+    
+    if embeddings_collection_name in collections:
+        _embeddings = _db[embeddings_collection_name]
+    else:
+        _embeddings = _db.create_collection(embeddings_collection_name, dimension=768, metric=VectorMetric.COSINE)
+
+    if files_collection_name in collections:
+        _files = _db[files_collection_name]
     else:
         _files = _db.create_collection(files_collection_name)
 
 
 def load_hashes():
-    """Return the document associated with the given path, or None if not found."""
-    return list(_files.paginated_find())
+    """Return all documents in the files collection."""
+    return list(_files.find({}))
 
 
 def file_by_id(file_id):
-    """Return the document with the given id.  Raises an exception if not found."""
-    return _files.find_one({'_id': file_id})['data']['document']
+    """Return the document with the given id. Raises an exception if not found."""
+    return _files.find_one({'_id': file_id})
 
 
 def delete(file_id):
     """Delete the file and embeddings documents associated with the given file"""
     _embeddings.delete_many({"file_id": file_id})
-    _files.delete_one(file_id)
+    _files.delete_one({"_id": file_id})
 
 
 def insert(file_id, full_path, chunks, encoded_chunks):
@@ -60,9 +59,11 @@ def insert(file_id, full_path, chunks, encoded_chunks):
     file_doc = {"path": full_path, "hash": hexdigest(full_path)}
     if file_id:
         file_doc["_id"] = file_id
-    file_id = _files.upsert_one(file_doc)
+    result = _files.insert_one(file_doc)
+    file_id = result.inserted_id
+    
     embeddings_docs = [{'file_id': file_id, 'chunk': chunk, '$vector': embedding}
-                 for chunk, embedding in zip(chunks, encoded_chunks)]
+                       for chunk, embedding in zip(chunks, encoded_chunks)]
     # call insert_many once per batch of 20 embeddings
     for i in range(0, len(embeddings_docs), 20):
         _embeddings.insert_many(embeddings_docs[i:i + 20])
@@ -70,4 +71,9 @@ def insert(file_id, full_path, chunks, encoded_chunks):
 
 def search(query_embedding, limit=10):
     """Return the top `limit` chunks that are most similar to the given query embedding."""
-    return _embeddings.vector_find(query_embedding, limit=limit, fields={"file_id", "chunk"})
+    return _embeddings.find(
+        {},
+        sort={"$vector": query_embedding},
+        limit=limit,
+        projection={"file_id": 1, "chunk": 1}
+    )
