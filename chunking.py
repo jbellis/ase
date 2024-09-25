dd6527f3259e0959b80e17d1a6f975252a9e114c
Jonathan Ellis <jbellis@datastax.com>
2024-09-25 13:26:59 -0500

    improve chunking for java

--- chunking.py
+++ chunking.py
@@ -3,7 +3,6 @@ import warnings
 warnings.simplefilter("ignore", category=FutureWarning)
 from tree_sitter_languages import get_parser
 
-
 def chunkify_code(code: str, language: str) -> List[str]:
     """
     Splits code into semantically meaningful chunks using tree-sitter.
@@ -12,16 +11,23 @@ def chunkify_code(code: str, language: str) -> List[str]:
     tree = parser.parse(bytes(code, "utf8"))
 
     chunks = []
-    for node in tree.root_node.children:
-        if node.type in ("function_definition", "method_definition", "function_declaration", "method_declaration"):
-            chunks.append(code[node.start_byte:node.end_byte])
-        elif node.type in ("class_definition", "class_declaration"):
+    
+    def traverse(node):
+        if node.type in ("class_declaration", "interface_declaration", "enum_declaration"):
+            # For classes, interfaces, and enums, we'll chunk their contents separately
             class_chunk = code[node.start_byte:node.end_byte]
             chunks.append(class_chunk)
-            
-            # Add methods as separate chunks
             for child in node.children:
-                if child.type in ("method_definition", "method_declaration", "function_definition", "function_declaration"):
-                    chunks.append(code[child.start_byte:child.end_byte])
+                traverse(child)
+        elif node.type in ("function_definition", "method_definition", "function_declaration", "method_declaration", "constructor_declaration"):
+            # TODO for C-like languages we only want to capture definitions, not declarations
+            method_chunk = code[node.start_byte:node.end_byte]
+            chunks.append(method_chunk)
+        else:
+            # For other node types, continue traversing
+            for child in node.children:
+                traverse(child)
 
+    traverse(tree.root_node)
+    
     return chunks
