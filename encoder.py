bcd4ed46ba38b36681e5e9e1674112a89a9ce7fb
Jonathan Ellis <jbellis@datastax.com>
2024-09-30 08:21:05 -0500

    jina wip

--- encoder.py
+++ encoder.py
@@ -3,19 +3,22 @@ import time
 from collections import deque
 
 import google.generativeai as gemini
+import requests
 
-gemini_key = os.environ["GEMINI_KEY"]
-if not gemini_key:
-    raise Exception('GEMINI_KEY environment variable not set')
-gemini.configure(api_key=gemini_key)
+JINA_API_KEY = os.environ.get("JINA_API_KEY")
+if not JINA_API_KEY:
+    raise Exception('JINA_API_KEY environment variable not set')
+
 
 class RateLimiter:
     def __init__(self, max_tokens, time_window):
         self.max_tokens = max_tokens
         self.time_window = time_window
         self.tokens = max_tokens
-        self.last_refill = time.time()
+        self.start_time = time.time()
+        self.last_refill = self.start_time
         self.requests = deque()
+        self.total_requests = 0
 
     def wait(self, tokens):
         self._refill()
@@ -24,6 +27,7 @@ class RateLimiter:
             self._refill()
         self.tokens -= tokens
         self.requests.append((time.time(), tokens))
+        self.total_requests += tokens
 
     def _refill(self):
         now = time.time()
@@ -34,10 +38,40 @@ class RateLimiter:
             _, tokens = self.requests.popleft()
             self.tokens = min(self.max_tokens, self.tokens + tokens)
 
-rate_limiter = RateLimiter(940, 60) # 1000 tokens per minute with a buffer
+    def status(self):
+        now = time.time()
+        elapsed = now - self.start_time
+        rps = self.total_requests / elapsed
+        return f"RateLimiter allowed {self.total_requests} requests in {elapsed}s = {rps:.2f} RPS"
 
+
+rate_limiter = RateLimiter(50, 60) # 1000 tokens per minute with a buffer
 def encode(inputs: list[str]) -> list[list[float]]:
-    rate_limiter.wait(len(inputs))
-    model = "models/text-embedding-004"
-    result = gemini.embed_content(model=model, content=inputs)
-    return result['embedding']
+    limited_inputs = [text[:30000] for text in inputs] # 8k tokens * 0.9 words/token * 4.7 bytes/word, conservatively
+    url = 'https://api.jina.ai/v1/embeddings'
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {JINA_API_KEY}"
+    }
+    data = {
+        "model": "jina-embeddings-v2-base-code",
+        "normalized": True,
+        "embedding_type": "float",
+        "input": limited_inputs
+    }
+
+    # write the request to a file for debugging
+    with open('/tmp/request.json', 'w') as f:
+        import json
+        f.write(json.dumps(data, indent=2))
+
+    rate_limiter.wait(1)
+    try:
+        response = requests.post(url, headers=headers, json=data)
+        response.raise_for_status()  # Raise an exception for HTTP errors
+    except:
+        print(rate_limiter.status())
+        raise
+
+    result = response.json()
+    return [item['embedding'] for item in result['data']]
